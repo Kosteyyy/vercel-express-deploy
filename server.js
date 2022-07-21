dc24a85f1fd43d4c01f47df4d97f23a95698dd61
Kosteyyy <averkiev@yandex.ru>
2022-07-21 15:54:25 +0300

    comment /here path add static public

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 // app.get("/", (req, res) => {
 //   res.send("Express on Vercel");
