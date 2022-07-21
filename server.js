8b5fd9363b66fc2e9184bf52021f06e92eae9e1a
Kosteyyy <averkiev@yandex.ru>
2022-07-21 15:52:50 +0300

    /here path no static public

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // app.get("/", (req, res) => {
 //   res.send("Express on Vercel");
 // });
-app.get("/here", (req, res) => {
-  res.sendFile(path.resolve("public/index.html"));
-});
+// app.get("/here", (req, res) => {
+//   res.sendFile(path.resolve("public/index.html"));
+// });
 
 app.listen(5000, () => console.log("Running on port 5000"));
 
