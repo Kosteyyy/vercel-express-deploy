fb2547e3cbb9371c5dc003bc1c4521db856fc5fd
Kosteyyy <averkiev@yandex.ru>
2022-07-21 15:57:54 +0300

    add img and /here path

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(express.static("public"));
 // app.get("/", (req, res) => {
 //   res.send("Express on Vercel");
 // });
-// app.get("/here", (req, res) => {
-//   res.sendFile(path.resolve("public/index.html"));
-// });
+app.get("/here", (req, res) => {
+  res.sendFile(path.resolve("public/index.html"));
+});
 
 app.listen(5000, () => console.log("Running on port 5000"));
 
